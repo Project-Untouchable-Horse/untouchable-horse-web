04b05259b9f6bc630eed55bc11955b5debd04d81
Jacob Farber <farber.102@osu.edu>
2025-04-05 23:05:43 -0400

    Add test cases

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -7,6 +7,22 @@ describe('The main app', () => {
   it('should render the Orders link', () => {
     render(<App />);
     expect(screen.getByText('Orders')).toBeInTheDocument();
-  })
+  });
 
-})
+  it('should render the Payments link', () => {
+    render(<App />);
+    expect(screen.getByText('Payment')).toBeInTheDocument();
+  });
+
+  it('should render the Home link', () => {
+    render(<App />);
+    //expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByRole('Link', { name:"Home" })).toBeInTheDocument();
+  });
+
+  it('should render the Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level:2})).toBeInTheDocument();
+  });
+
+});
